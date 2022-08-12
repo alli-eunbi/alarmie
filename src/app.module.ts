1ef7133e22cbf89412ad6d22d54e1765c992a0d5
alli-eunbi <allieunbi@gmail.com>
2022-08-12 16:49:23 +0900

    로컬 디비 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: '1234',
       database: 'example',
       entities: [],
       synchronize: true,
